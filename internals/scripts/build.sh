d6218094d2f90c9b642dc0b45f8b01d16bfe4ba1
Nick Schmidt <nick@kreativgebiet.com>
2018-12-04 14:42:39 +0100

    chore: clear files before publishing

--- internals/scripts/build.sh
+++ internals/scripts/build.sh
@@ -2,6 +2,8 @@
 
 if [ "$1" != "" ]; then
   if [ -e "./packages/$1/package.json" ]; then
+    ./node_modules/rimraf/bin.js $1/es $1/lib
+
     ./node_modules/@babel/cli/bin/babel.js ./packages/$1/src --root-mode upward --out-dir ./packages/$1/es
     BABEL_ENV=commonjs ./node_modules/@babel/cli/bin/babel.js ./packages/$1/src --root-mode upward --out-dir ./packages/$1/lib
   else
@@ -12,6 +14,9 @@ else
     package=`basename $f`
 
     if [ -d "$f" ] && [ -d "$f/src" ] && [ -e "$f/package.json" ]; then
+      echo "$f will be compiled now."
+      ./node_modules/rimraf/bin.js $f/es $f/lib
+
       ./node_modules/@babel/cli/bin/babel.js $f/src --root-mode upward --out-dir $f/es
       BABEL_ENV=commonjs ./node_modules/@babel/cli/bin/babel.js $f/src --root-mode upward --out-dir $f/lib
     fi
