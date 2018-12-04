2d21eac66bdff01a9e96eda0b8f12e3f538f653c
Nick Schmidt <nick@kreativgebiet.com>
2018-12-04 14:04:04 +0100

    feat(utils): replace `test` with `it` methods

--- packages/toolkit-utils/test/dynamicFontSize.test.js
+++ packages/toolkit-utils/test/dynamicFontSize.test.js
@@ -2,22 +2,22 @@ import fontSize, { generateFontSize } from '../src/dynamicFontSize'
 
 describe('dynamicFontSize', () => {
   describe('fontSize', () => {
-    test('returns a function', () => {
+    it('returns a function', () => {
       expect(typeof fontSize).toEqual('function')
     })
 
-    test('function returns an css literal', () => {
+    it('function returns an css literal', () => {
       const css = fontSize(16)
       expect(typeof css).toEqual('object')
     })
   })
 
   describe('generateFontSize', () => {
-    test('returns a function', () => {
+    it('returns a function', () => {
       expect(typeof generateFontSize).toEqual('function')
     })
 
-    test('returns an object with right numbers', () => {
+    it('returns an object with right numbers', () => {
       const value = generateFontSize(16)
 
       expect(value.fontSize).toEqual(16)
@@ -25,7 +25,7 @@ describe('dynamicFontSize', () => {
       expect(value.letterSpacing).toEqual('-0.005')
     })
 
-    test('accepts second attribute for line height', () => {
+    it('accepts second attribute for line height', () => {
       const value = generateFontSize(16, 1.5)
 
       expect(value.fontSize).toEqual(16)
